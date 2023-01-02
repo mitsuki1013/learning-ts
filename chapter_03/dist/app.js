c3fa9e40b3c62b5cc6fc5eeb5e81d1cff3b26b43
mitsuki1013 <mitsuki.highjump@gmail.com>
2023-01-03 02:52:33 +0900

    chapter_4: 新しい世代のJavaScriptとTypeScriptについて

--- chapter_03/dist/app.js
+++ chapter_03/dist/app.js
@@ -1,3 +1,29 @@
 "use strict";
-console.log("chapter_03");
+const button = document.querySelector("button");
+if (button) {
+    button.addEventListener("click", (event) => {
+        console.log(event);
+    });
+}
+const hobbies = ["Sports", "Cooking", "Hacking", "Guitar"];
+const activeHobbies = ["Hiking", ...hobbies];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+const person = {
+    firstName: "Max",
+    age: 30,
+};
+const copiedPerson = Object.assign({}, person);
+console.log(copiedPerson);
+const add = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2, remainingHobbies);
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
 //# sourceMappingURL=app.js.map
--- chapter_03/src/app.ts
+++ chapter_03/src/app.ts
@@ -1,1 +1,52 @@
-console.log("chapter_03");
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput: (output: string | number) => void = (output) => {
+//   console.log(output);
+// };
+
+// printOutput(add(5));
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => {
+    console.log(event);
+  });
+}
+
+const hobbies = ["Sports", "Cooking", "Hacking", "Guitar"];
+
+// スプレッドオペレータ（配列）
+const activeHobbies = ["Hiking", ...hobbies];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person = {
+  firstName: "Max",
+  age: 30,
+};
+
+// スプレッドオペレータ（オブジェクト）
+const copiedPerson = {
+  ...person,
+};
+
+console.log(copiedPerson);
+
+// レストパラメータ
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+// 分割代入（配列）
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobbies, hobby1, hobby2, remainingHobbies);
+
+// 分割代入(オブジェクト)
+const { firstName: userName, age } = person;
+console.log(userName, age, person);
